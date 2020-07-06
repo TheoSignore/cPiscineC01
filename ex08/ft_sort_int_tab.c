0740290ee9eca7939bafdf1bba7585f0b8ef4164
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-06 12:20:37 +0200

    ex08 done

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/06 10:55:52 by tsignore          #+#    #+#             */
+/*   Updated: 2020/07/06 12:12:44 by tsignore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (tab[j] > tab[j + 1])
+				ft_swap(&tab[j], &tab[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
